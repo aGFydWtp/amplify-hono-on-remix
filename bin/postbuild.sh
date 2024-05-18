cdd426b2f7df6db63f7651a9801e447988f51344
aGFydWtp <haruki@censor.space>
2024-05-18 08:18:18 +0000

    chore: add husky

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -5,12 +5,11 @@ rm -rf ./.amplify-hosting
 mkdir -p ./.amplify-hosting/compute/default
 
 cp -r ./build ./.amplify-hosting/compute/default/build
-cp -r bin/package.json ./.amplify-hosting/compute/default
+cp -r ./package.json ./.amplify-hosting/compute/default
 
 cp -r public ./.amplify-hosting/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
 
-rm -rf node_modules
-yarn install --production
-cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+cd .amplify-hosting/compute/default
+bun install --production
